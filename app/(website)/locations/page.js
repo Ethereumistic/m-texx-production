2d024414f65dcc664f3bb8a1d6c44a0851ec1334
Ethereumistic <badjarovv@gmail.com>
2023-08-23 18:13:56 +0300

    removed stuff from locations since bugs 2

--- app/(website)/locations/page.js
+++ app/(website)/locations/page.js
@@ -1,25 +1,7 @@
-"use client";
-import dynamic from "next/dynamic";
-import Container from "@/components/container";
-import { React, useState, useRef, useEffect } from "react";
-import { FaAngleDown, FaAngleRight } from 'react-icons/fa';
-import SideMenu from "@/components/SideMenu";
-import MarkerGrid from "@/components/MarkerGrid";
 
-import { Icon } from 'leaflet';
-import "leaflet/dist/leaflet.css";
 
-import Backlink from "@/components/Backlink";
+import { React } from "react";
 
-const DynamicMap = dynamic(() => import('@/components/DesktopMap'), {
-  ssr: false,
-})
-
-const markers = require('@/components/markers');
-const customIcon = new Icon({
-  iconUrl: "https://cdn.discordapp.com/attachments/1017047163324346420/1138806234468843570/location.png",
-  iconSize: [34, 34]
-})
 
 
 
@@ -29,7 +11,7 @@ export default function Locations() {
 
 
   return (
-    <div>v kontakti</div>
+    <div>v kontaktis</div>
 
   );
 }
