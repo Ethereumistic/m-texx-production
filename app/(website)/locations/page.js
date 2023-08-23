f1b4ffb1fa07fb07d004433e94577818397e66f0
Ethereumistic <badjarovv@gmail.com>
2023-08-23 17:33:58 +0300

    adjusting location trying map without ssr false

--- app/(website)/locations/page.js
+++ app/(website)/locations/page.js
@@ -1,9 +1,85 @@
-import React from 'react'
+"use client";
+import dynamic from "next/dynamic";
+import Container from "@/components/container";
+import { React, useState, useRef, useEffect } from "react";
 
-const Locations = () => {
+import SideMenu from "@/components/SideMenu";
+import MarkerGrid from "@/components/MarkerGrid";
+
+import { Icon } from 'leaflet';
+import "leaflet/dist/leaflet.css";
+
+import { MapContainer, TileLayer, Marker, Popup, useMap, mapConsumer } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-cluster';
+
+
+
+
+
+const markers = require('@/components/markers');
+const customIcon = new Icon({
+  iconUrl: "https://cdn.discordapp.com/attachments/1017047163324346420/1138806234468843570/location.png",
+  iconSize: [34, 34]
+})
+
+
+
+
+export default function Locations() {
+
+
+
+
   return (
-    <div>Locations</div>
-  )
-}
 
-export default Locations
+    <Container className="">
+      <div >
+        <div className="">
+
+          {/* MAP */}
+          <div className="" >
+          <div className='w-full'>
+        <div className="">
+        
+
+        <MapContainer
+      center={[42.7339, 25.4858]}
+      zoom={8}
+      style={{ width: '100%', height: '800px', zIndex: '10' }} >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+<MarkerClusterGroup
+          chunkedLoading>
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker.geocode} icon={customIcon}>
+            <Popup>
+  <div>
+    <p>{marker.city}</p>
+    <p>{marker.popUp}</p>
+    <p>
+{marker.geocode[0].toFixed(6)} {marker.geocode[1].toFixed(6)}
+    </p>
+  </div>
+</Popup>
+
+
+          </Marker>
+        ))}
+        </MarkerClusterGroup>
+
+
+
+        </MapContainer>
+        asd
+        </div>
+        </div>
+          </div>
+          {/* MAP */}
+
+          <SideMenu />
+          <MarkerGrid markers={markers} />
+        </div>
+      </div>
+    </Container>
+
+  );
+}
