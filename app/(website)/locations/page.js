d14faa0a62f97fddd408ddd70993681afbe3ccbc
Ethereumistic <badjarovv@gmail.com>
2023-08-24 13:32:14 +0300

    added dynamiclokacii in locations page

--- app/(website)/locations/page.js
+++ app/(website)/locations/page.js
@@ -1,9 +1,12 @@
 
 
 import { React } from "react";
+import dynamic from "next/dynamic";
+import Lokacii from "@/components/Lokacii";
 
-
-
+const DynamicLokacii = dynamic(() => import('@/components/Lokacii'), {
+  ssr: false,
+})
 
 
 export default function Locations() {
@@ -11,7 +14,9 @@ export default function Locations() {
 
 
   return (
-    <div>v kontaktis</div>
+    <div>
+      <DynamicLokacii />
+    </div>
 
   );
 }
