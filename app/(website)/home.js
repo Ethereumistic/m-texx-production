a38210f71ee79774599cf2b5292783232699dc13
Ethereumistic <badjarovv@gmail.com>
2023-10-09 19:22:32 +0300

    added video bg for phone and pc

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -35,14 +35,18 @@ export default function Post({ posts }) {
       )} */}
       {/* <div className="text-center text-4xl">САМО ЛОКАЦИИТЕ РАБОТЯТ</div> */}
       <div className="relative ">
-        <video className="rounded-lg -mt-6" src="/img/bgvideo.webm" autoPlay loop style={{ filter: 'brightness(0.6)' }}></video>
+        {/* Mobile Background Video */}
+        <video className="-mt-6 brightness-75 md:hidden" src="/img/background2.mp4" autoPlay loop muted playsInline></video>
+        {/* Desktop Background Video */}
+        <video className="hidden md:block xl:rounded-lg -mt-6 brightness-75" src="/img/bgvideo.webm" autoPlay loop muted playsInline></video>
+
 
           <div class="absolute inset-0 flex items-center justify-center">
             <div class="text-center">
-              <h1 class="text-5xl text-white font-bold text-right">Рециклиране на текстил: Втори живот за стари дрехи, 
+              <h1 class="mx-16 text-md xl:text-5xl text-white font-bold text-right">Рециклиране на текстил: Втори живот за стари дрехи, 
               <br />
               първа стъпка към...</h1>
-              <button class="text-2xl mt-2 bg-transparent border border-white hover:bg-green-500 hover:border-green-500 text-white py-4 px-8 rounded-full float-right">Рециклиране на Текстил</button>
+              <button class="text-md xl:text-2xl mr-4 mt-4 bg-transparent border border-white hover:bg-green-500 hover:border-green-500 text-white py-1 px-2 xl:py-4 xl:px-8 rounded-full float-right">Рециклиране на Текстил</button>
             </div>
           </div>
 
--- app/(website)/markerc/page.js
+++ app/(website)/markerc/page.js
@@ -1,68 +1,27 @@
-"use client";
-import React, { useState } from 'react';
-import markers from 'components/markerz'; // Import your existing markers array.
-import localStorage from 'local-storage';
-
-export default function MarkerForm() {
-  const [city, setCity] = useState('');
-  const [popUp, setPopUp] = useState('');
-  const [geocode, setGeocode] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Create a new marker object with the input values.
-    const newMarker = {
-      city,
-      popUp,
-      geocode: geocode.split(',').map(Number),
-    };
-
-    // Add the newMarker to the markers array.
-    markers.push(newMarker);
-
-    localStorage.setItem('markers', JSON.stringify(markers));
+import React from 'react';
+import Image from 'next/image';
+import { FaLightbulb } from 'react-icons/fa';
 
 
-    // Clear the form inputs.
-    setCity('');
-    setPopUp('');
-    setGeocode('');
-  };
-
+const MarkerC = () => {
   return (
     <div>
-      <h1>Add a Marker</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="city">City:</label>
-          <input
-            type="text"
-            id="city"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="popUp">Pop Up:</label>
-          <input
-            type="text"
-            id="popUp"
-            value={popUp}
-            onChange={(e) => setPopUp(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="geocode">Geocode (Lat,Lng):</label>
-          <input
-            type="text"
-            id="geocode"
-            value={geocode}
-            onChange={(e) => setGeocode(e.target.value)}
-          />
-        </div>
-        <button type="submit">Add Marker</button>
-      </form>
+
+      <div className="relative ">
+        <video className=" xl:rounded-lg -mt-6 brightness-75" src="/img/background2.mp4" autoPlay loop muted playsInline></video>
+
+          <div class="absolute inset-0 flex items-center justify-center">
+            <div class="text-center">
+              <h1 class="mx-16 text-md xl:text-5xl text-white font-bold text-right">Рециклиране на текстил: Втори живот за стари дрехи, 
+              <br />
+              първа стъпка към...</h1>
+              <button class="text-md xl:text-2xl mt-4 bg-transparent border border-white hover:bg-green-500 hover:border-green-500 text-white py-1 px-2 xl:py-4 xl:px-8 rounded-full float-right">Рециклиране на Текстил</button>
+            </div>
+          </div>
+
+      </div>
     </div>
   );
-}
+};
+
+export default MarkerC;
