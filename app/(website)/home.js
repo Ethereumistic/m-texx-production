a7657729e2636c25e4c33121c7b4027b74320180
Ethereumistic <badjarovv@gmail.com>
2024-02-08 22:31:44 +0200

    bug with sanity i hope fixed

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -17,7 +17,7 @@ export default function Post({ posts }) {
   return (
     <>
     
-      {/* {posts && (
+    {/* {posts && (
         <Container>
           <div className="grid gap-10 md:grid-cols-2 lg:gap-10 ">
             {posts.slice(0, 2).map(post => (
@@ -37,12 +37,13 @@ export default function Post({ posts }) {
           <div className="mt-10 flex justify-center">
             <Link
               href="/archive"
-              className="relative inline-flex items-center gap-1 rounded-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-dgreen dark:text-gray-300">
+              className="relative inline-flex items-center gap-1 rounded-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300">
               <span>View all Posts</span>
             </Link>
           </div>
         </Container>
       )} */}
+
       {/* <div className="text-center text-4xl">САМО ЛОКАЦИИТЕ РАБОТЯТ</div> */}
       <div className="relative ">
         {/* Mobile Background Video */}
